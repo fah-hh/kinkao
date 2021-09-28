b2315d07c5b628a72cc43c8b161a00b29f1a4ab8
kanravee <kanravee.w@ku.th>
2021-09-28 10:21:36 +0700

    kinkao

---
+++ src/main/java/th/ac/ku/kinkao/controller/RestaurantController.java
@@ -0,0 +1,38 @@
+package th.ac.ku.kinkao.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.ku.kinkao.model.Restaurant;
+import th.ac.ku.kinkao.service.RestaurantService;
+
+@Controller
+@RequestMapping("/restaurant")
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantService service;
+
+    @GetMapping
+    public String getRestaurants(Model model) {
+        model.addAttribute("restaurants", service.getAll());
+        return "restaurants";
+    }
+
+    @GetMapping("/add")
+    public String getAddForm() {
+        // return restaurant-add.html
+        return "restaurant-add";
+    }
+
+    @PostMapping("/add")
+    public String addRestaurant(@ModelAttribute Restaurant restaurant, Model model) {
+        service.addRestaurant(restaurant);
+        // กลับไปหน้าไหน.. อันนี้คือ home
+        return "redirect:/restaurant";
+    }
+}
